d393cb0f8054fafa77c016481cbc6d824df749d6
Efis_dd45 <Efis_dd45@student.itstep.org>
2023-01-01 16:38:41 +0200

    01-01-2023

---
+++ HelloGitHub_Csh/HelloGitHub_Csh/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HelloGitHub_Csh {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, GitHub!");
+            Console.ReadKey();
+        }
+    }
+}
